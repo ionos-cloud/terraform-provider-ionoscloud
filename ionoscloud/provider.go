9ebc882b0749913bc78a7091f0e2622d87cd7aeb
Florin Mihalache <florin.mihalache@ionos.com>
2021-03-25 15:49:52 +0200

    fix: computing schema and host

--- ionoscloud/provider.go
+++ ionoscloud/provider.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform-plugin-sdk/httpclient"
 	"log"
+	"strings"
 	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -142,7 +143,13 @@ func providerConfigure(d *schema.ResourceData, terraformVersion string) (interfa
 
 	newConfig := ionoscloud.NewConfiguration(username.(string), password.(string), token.(string))
 	if len(cleanedUrl) > 0 {
-		newConfig.Host = cleanedUrl
+		parts := strings.Split(cleanedUrl, "://")
+		if len(parts) == 1 {
+			newConfig.Host = cleanedUrl
+		} else {
+			newConfig.Scheme = parts[1]
+			newConfig.Host = parts[2]
+		}
 	}
 	newConfig.UserAgent = httpclient.TerraformUserAgent(terraformVersion)
 	newClient := ionoscloud.NewAPIClient(newConfig)
