2704abecfc0eacbdc9980a6cca4530f6e12a0c28
IuliaBlindu <iuliaioana.blindu@gmail.com>
2021-07-26 17:04:06 +0300

    group autoscaling in progress

---
+++ ionoscloud/resource_autoscaling_group.go
@@ -0,0 +1,231 @@
+package ionoscloud
+
+import (
+	"context"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceAutoscalingGroup() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: resourceAutoscalingGroupCreate,
+		ReadContext:   resourceAutoscalingGroupRead,
+		UpdateContext: resourceAutoscalingGroupUpdate,
+		DeleteContext: resourceAutoscalingGroupDelete,
+		Schema: map[string]*schema.Schema{
+			"datacenter": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Description: "VMs for this Autoscaling Group will be created in this Virtual Datacenter. Please note, that it have the same `location` as the `template`.",
+				Required:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"href": {
+							Type:        schema.TypeString,
+							Description: "Absolute URL to the resource's representation",
+							Computed:    true,
+						},
+						"type": {
+							Type:        schema.TypeString,
+							Description: "Type of resource",
+							Computed:    true,
+						},
+						"id": {
+							Type:         schema.TypeString,
+							Description:  "Unique identifier for the resource",
+							Required:     true,
+							ValidateFunc: validation.All(validation.StringIsNotWhiteSpace),
+						},
+					},
+				},
+			},
+			"location": {
+				Type:        schema.TypeString,
+				Description: "Location of the datacenter. This location is the same as the one from the selected template.",
+				Computed:    true,
+			},
+			"max_replica_count": {
+				Type:        schema.TypeInt,
+				Description: "Maximum replica count value for `targetReplicaCount`. Will be enforced for both automatic and manual changes.",
+				Required:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(int)
+					if v < 0 || v > 200 {
+						errs = append(errs, fmt.Errorf("%q must be between 0 and 200 inclusive, got: %d", key, v))
+					}
+					return
+				},
+			},
+			"min_replica_count": {
+				Type:        schema.TypeInt,
+				Description: "Minimum replica count value for `targetReplicaCount`. Will be enforced for both automatic and manual changes.",
+				Required:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(int)
+					if v < 0 || v > 200 {
+						errs = append(errs, fmt.Errorf("%q must be between 0 and 200 inclusive, got: %d", key, v))
+					}
+					return
+				},
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Description: "User-defined name for the Autoscaling Group.",
+				Optional:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(string)
+					if len(v) > 255 {
+						errs = append(errs, fmt.Errorf("%q must have the maximum length of 255, and it actually has: %d", key, len(v)))
+					}
+					return
+				},
+			},
+			"policy": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Description: "Specifies the behavior of this Autoscaling Group. A policy consists of Triggers and Actions, whereby an Action is some kind of automated behavior, and a Trigger is defined by the circumstances under which the Action is triggered. Currently, two separate Actions, namely Scaling In and Out are supported, triggered through Thresholds defined on a given Metric.",
+				Required:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"metric": {
+							Required:    true,
+							Type:        schema.TypeString,
+							Description: "The Metric that should trigger Scaling Actions. The values of the Metric are checked in fixed intervals.",
+						},
+						"range": {
+							Required:    true,
+							Type:        schema.TypeString,
+							Description: "Defines the range of time from which samples will be aggregated. Default is 120s.",
+						},
+						"scale_in_action": {
+							Required:    true,
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Description: "Specifies the Action to take when the `scaleInThreshold` is exceeded. Hereby, scaling in is always about removing VMs that are currently associated with this Autoscaling Group.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"amount": {
+										Required:    true,
+										Type:        schema.TypeInt,
+										Description: "When `amountType == ABSOLUTE`, this is the number of VMs added or removed in one step. When `amountType == PERCENTAGE`, this is a percentage value, which will be applied to the Autoscaling Group's current `targetReplicaCount` in order to derive the number of VMs that will be added or removed in one step. There will always be at least one VM added or removed.",
+									},
+									"amount_type": {
+										Required:    true,
+										Type:        schema.TypeString,
+										Description: "The type for the given amount. Possible values are: [ABSOLUTE, PERCENTAGE].",
+									},
+									"cooldown_period": {
+										Required:    true,
+										Type:        schema.TypeString,
+										Description: "Minimum time to pass after this Scaling Action has started, until the next Scaling Action will be started. Additionally, if a Scaling Action is currently in progress, no second Scaling Action will be started for the same Autoscaling Group. Instead, the Metric will be re-evaluated after the current Scaling Action completed (either successful or with failures).",
+									},
+								},
+							},
+						},
+						"scale_in_threshold": {
+							Required:    true,
+							Type:        schema.TypeInt,
+							Description: "A lower threshold on the value of `metric`. Will be used with `less than` (<) operator. Exceeding this will start a Scale-In Action as specified by the `scaleInAction` property. The value must have a higher minimum delta to the `scaleOutThreshold` depending on the `metric` to avoid competitive actions at the same time.",
+						},
+						"scale_out_action": {
+							Required:    true,
+							Type:        schema.TypeList,
+							MaxItems:    1,
+							Description: "Specifies the Action to take when the `scaleInThreshold` is exceeded. Hereby, scaling in is always about removing VMs that are currently associated with this Autoscaling Group.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"amount": {
+										Required:    true,
+										Type:        schema.TypeInt,
+										Description: "When `amountType == ABSOLUTE`, this is the number of VMs added or removed in one step. When `amountType == PERCENTAGE`, this is a percentage value, which will be applied to the Autoscaling Group's current `targetReplicaCount` in order to derive the number of VMs that will be added or removed in one step. There will always be at least one VM added or removed.",
+									},
+									"amount_type": {
+										Required:    true,
+										Type:        schema.TypeString,
+										Description: "The type for the given amount. Possible values are: [ABSOLUTE, PERCENTAGE].",
+									},
+									"cooldown_period": {
+										Required:    true,
+										Type:        schema.TypeString,
+										Description: "Minimum time to pass after this Scaling Action has started, until the next Scaling Action will be started. Additionally, if a Scaling Action is currently in progress, no second Scaling Action will be started for the same Autoscaling Group. Instead, the Metric will be re-evaluated after the current Scaling Action completed (either successful or with failures).",
+									},
+								},
+							},
+						},
+						"scale_out_threshold": {
+							Required:    true,
+							Type:        schema.TypeInt,
+							Description: "A lower threshold on the value of `metric`. Will be used with `less than` (<) operator. Exceeding this will start a Scale-In Action as specified by the `scaleInAction` property. The value must have a higher minimum delta to the `scaleOutThreshold` depending on the `metric` to avoid competitive actions at the same time.",
+						},
+						"unit": {
+							Required:    true,
+							Type:        schema.TypeString,
+							Description: "Specifies the Action to take when the `scaleInThreshold` is exceeded. Hereby, scaling in is always about removing VMs that are currently associated with this Autoscaling Group.",
+						},
+					},
+				},
+			},
+			"target_replica_count": {
+				Type:        schema.TypeInt,
+				Description: "The target number of VMs in this Group. Depending on the scaling policy, this number will be adjusted automatically. VMs will be created or destroyed automatically in order to adjust the actual number of VMs to this number. This value can be set only at Group creation time, subsequent change via update (PUT) request is not possible.",
+				Required:    true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(int)
+					if v < 0 || v > 200 {
+						errs = append(errs, fmt.Errorf("%q must be between 0 and 200 inclusive, got: %d", key, v))
+					}
+					return
+				},
+			},
+			"template": {
+				Type:        schema.TypeList,
+				MaxItems:    1,
+				Description: "VMs for this Autoscaling Group will be created in this Virtual Datacenter. Please note, that it have the same `location` as the `template`.",
+				Required:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"href": {
+							Type:        schema.TypeString,
+							Description: "Absolute URL to the resource's representation",
+							Computed:    true,
+						},
+						"type": {
+							Type:        schema.TypeString,
+							Description: "Type of resource",
+							Computed:    true,
+						},
+						"id": {
+							Type:         schema.TypeString,
+							Description:  "Unique identifier for the resource",
+							Required:     true,
+							ValidateFunc: validation.All(validation.StringIsNotWhiteSpace),
+						},
+					},
+				},
+			},
+		},
+		Timeouts: &resourceDefaultTimeouts,
+	}
+}
+
+func resourceAutoscalingGroupCreate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	return resourceAutoscalingGroupRead(ctx, d, meta)
+}
+
+func resourceAutoscalingGroupRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	return nil
+}
+
+func resourceAutoscalingGroupUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	return resourceAutoscalingGroupRead(ctx, d, meta)
+}
+
+func resourceAutoscalingGroupDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+
+	return nil
+}
