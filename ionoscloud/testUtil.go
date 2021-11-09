22c51e7da3efe01335158fdbd239c17771f9fd69
cristi <cristian.mihai.guran@ionos.com>
2021-11-09 10:11:06 +0200

    SDK-560 Add unit test for nic datasource read

---
+++ ionoscloud/testUtil.go
@@ -0,0 +1,39 @@
+package ionoscloud
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	ionoscloud "github.com/ionos-cloud/sdk-go/v6"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+//mocked server, that responds with jsonResponse to whatever we send to it
+func createMockServer(jsonResponse string) *httptest.Server {
+	ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		_, err := fmt.Fprintln(w, jsonResponse)
+		if err != nil {
+			log.Println("error while writing to server", err)
+		}
+	}))
+	return ts
+}
+
+//we need to connect the mocked server to our httpClient, so we inject a mocked client into cfg
+func getMockedClient(jsonResponse string) interface{} {
+	ts := createMockServer(jsonResponse)
+
+	cfg := ionoscloud.NewConfiguration("", "", "", ts.URL)
+	cfg.HTTPClient = ts.Client()
+
+	return ionoscloud.NewAPIClient(cfg)
+}
+
+func getEmptyTestResourceData(t *testing.T, resourceSchema map[string]*schema.Schema) *schema.ResourceData {
+	testMap := map[string]interface{}{}
+	var testSchema = resourceSchema
+	return schema.TestResourceDataRaw(t, testSchema, testMap)
+}
