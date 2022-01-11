02e4eec8de9e742662e30eef624ee0f33da3f18c
Iulia Blindu <35154453+IuliaBlindu@users.noreply.github.com>
2022-01-11 14:00:09 +0200

    fix: fixed lan deletion - wait for completion of nic deletion (#181)

--- ionoscloud/data_source_image_test.go
+++ ionoscloud/data_source_image_test.go
@@ -6,7 +6,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-func TestAccDataSourceImage_basic(t *testing.T) {
+func TestAccDataSourceImageBasic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck: func() {
 			testAccPreCheck(t)
