fe7ee42061c00e6dd427d521920371c4577d6b23
Iulia Blindu <35154453+IuliaBlindu@users.noreply.github.com>
2022-04-01 16:55:13 +0300

    enhancement: IONOS_USERNAME, IONOS_PASSWORD and IONOS_TOKEN can be set in the same time (#258)

--- ionoscloud/provider_test.go
+++ ionoscloud/provider_test.go
@@ -44,11 +44,6 @@ func testAccPreCheck(t *testing.T) {
 		if pbUsername == "" || pbPassword == "" {
 			t.Fatalf("%s/%s or %s must be set for acceptance tests", ionoscloud.IonosUsernameEnvVar, ionoscloud.IonosPasswordEnvVar, ionoscloud.IonosTokenEnvVar)
 		}
-	} else {
-		if pbUsername != "" || pbPassword != "" {
-			t.Fatalf("%s/%s can not be set together with %s", ionoscloud.IonosUsernameEnvVar, ionoscloud.IonosPasswordEnvVar, ionoscloud.IonosTokenEnvVar)
-		}
-
 	}
 
 	diags := testAccProvider.Configure(context.TODO(), terraform.NewResourceConfigRaw(nil))
