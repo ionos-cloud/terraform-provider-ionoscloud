7f6542d8e4f77937ea1009b0acbd1f2dddd26ede
cristi <cristian.mihai.guran@ionos.com>
2023-06-19 14:45:56 +0300

    merge master

--- ionoscloud/resource_user.go
+++ ionoscloud/resource_user.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	ionoscloud "github.com/ionos-cloud/sdk-go/v6"
+	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/slice"
 	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/utils"
 	"log"
 )
