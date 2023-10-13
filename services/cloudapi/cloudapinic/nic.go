6805856741a5ebc2d59fb73576ac7a15dd4a7a80
cristi <cristian.mihai.guran@ionos.com>
2023-10-13 12:17:55 +0300

    fix: return empty list on err

--- services/cloudapi/cloudapinic/nic.go
+++ services/cloudapi/cloudapinic/nic.go
@@ -18,13 +18,15 @@ type Service struct {
 }
 
 func (fs *Service) List(ctx context.Context, datacenterID, serverID string, depth int32) ([]ionoscloud.Nic, error) {
+	emptyNicList := make([]ionoscloud.Nic, 0)
 	nics, apiResponse, err := fs.Client.NetworkInterfacesApi.DatacentersServersNicsGet(ctx, datacenterID, serverID).Depth(depth).Execute()
 	apiResponse.LogInfo()
 	if err != nil {
-		return nil, err
+		return emptyNicList, err
 	}
 	if nics.Items == nil {
-		return make([]ionoscloud.Nic, 0), fmt.Errorf("nic list is empty for datacenter %s, server %s", datacenterID, serverID)
+		log.Printf("[DEBUG] empty nic list for datacenter %s, server %s", datacenterID, serverID)
+		return emptyNicList, nil
 	}
 	return *nics.Items, nil
 }
