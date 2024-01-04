aa98b64e329bec6c7f7f14dfe4faaa2624199840
ionutb <corneliu-ionut.beti@ionos.com>
2024-01-04 12:27:33 +0200

    remove 'cube' tag from enterprise tests

--- ionoscloud/resource_server_test.go
+++ ionoscloud/resource_server_test.go
@@ -1,5 +1,5 @@
-//go:build compute || all || server || enterprise || cube
-// +build compute all server enterprise cube
+//go:build compute || all || server || enterprise
+// +build compute all server enterprise
 
 package ionoscloud
 
