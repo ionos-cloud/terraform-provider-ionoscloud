2a1925a2a42c579f8a4b9e699dbfb86681c5db34
Gabriela Limberea <gabriela.limberea@ionos.com>
2024-06-11 12:41:46 +0300

    update kafka cluster resource

--- ionoscloud/resource_kafka_cluster.go
+++ ionoscloud/resource_kafka_cluster.go
@@ -16,7 +16,6 @@ func resourceKafkaCluster() *schema.Resource {
 	return &schema.Resource{
 		CreateContext: resourceKafkaClusterCreate,
 		ReadContext:   resourceKafkaClusterRead,
-		UpdateContext: resourceKafkaClusterUpdate,
 		DeleteContext: resourceKafkaClusterDelete,
 		Importer: &schema.ResourceImporter{
 			StateContext: resourceKafkaClusterImport,
@@ -27,18 +26,59 @@ func resourceKafkaCluster() *schema.Resource {
 				Type:             schema.TypeString,
 				Description:      "The name of your Kafka cluster. Must be 63 characters or less and must begin and end with an alphanumeric character (`[a-z0-9A-Z]`) with dashes (`-`), underscores (`_`), dots (`.`), and alphanumerics between.",
 				Required:         true,
+				ForceNew:         true,
 				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotWhiteSpace),
 			},
 			"version": {
 				Type:        schema.TypeString,
 				Description: "The desired Kafka Version. Supported version: 3.5.1",
+				ForceNew:    true,
 				Required:    true,
 			},
 			"size": {
 				Type:             schema.TypeString,
 				Description:      "The size of your Kafka cluster. The size of the Kafka cluster is given in T-shirt sizes. Valid values are: S",
 				Required:         true,
+				ForceNew:         true,
 				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotWhiteSpace),
+			},
+			"connections": {
+				Type:        schema.TypeList,
+				MinItems:    1,
+				MaxItems:    1,
+				Description: "The network connection for your cluster. Only one connection is allowed.",
+				Required:    true,
+				ForceNew:    true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"datacenter_id": {
+							Type:             schema.TypeString,
+							Description:      "The datacenter to connect your Kafka cluster to.",
+							Required:         true,
+							ValidateDiagFunc: validation.ToDiagFunc(validation.IsUUID),
+						},
+						"lan_id": {
+							Type:             schema.TypeString,
+							Description:      "The numeric LAN ID to connect your Kafka cluster to.",
+							Required:         true,
+							ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotWhiteSpace),
+						},
+						"cidr": {
+							Type:             schema.TypeString,
+							Description:      "The IP and subnet for your Kafka cluster.",
+							Required:         true,
+							ValidateDiagFunc: validation.ToDiagFunc(VerifyUnavailableIPs),
+						},
+						"broker_addresses": {
+							Type:        schema.TypeList,
+							Description: "The broker addresses of the Kafka Cluster",
+							Required:    false,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+					},
+				},
 			},
 		},
 		Timeouts: &resourceDefaultTimeouts,
@@ -86,31 +126,6 @@ func resourceKafkaClusterRead(ctx context.Context, d *schema.ResourceData, meta 
 	}
 
 	return nil
-}
-
-func resourceKafkaClusterUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-
-	client := meta.(services.SdkBundle).KafkaClient
-
-	_, apiResponse, err := client.UpdateCluster(ctx, d.Id(), d)
-	apiResponse.LogInfo()
-
-	if err != nil {
-		if apiResponse.HttpNotFound() {
-			d.SetId("")
-			return nil
-		}
-		diags := diag.FromErr(fmt.Errorf("error while updating kafka cluster: %w", err))
-		return diags
-	}
-
-	err = utils.WaitForResourceToBeReady(ctx, d, client.IsClusterAvailable)
-	if err != nil {
-		diags := diag.FromErr(fmt.Errorf("an error occured while kafka cluster waiting to be ready: %w", err))
-		return diags
-	}
-
-	return resourceKafkaClusterRead(ctx, d, meta)
 }
 
 func resourceKafkaClusterDelete(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
--- services/kafka/cluster.go
+++ services/kafka/cluster.go
@@ -15,9 +15,12 @@ import (
 
 var ClusterResourceName = "Kafka"
 
-func (c *Client) CreateCluster(ctx context.Context, d *schema.ResourceData) (kafka.KafkaPostPutResponseData, utils.ApiResponseInfo, error) {
+func (c *Client) CreateCluster(ctx context.Context, d *schema.ResourceData) (
+	kafka.ClusterRead, utils.ApiResponseInfo,
+	error,
+) {
 	request := setClusterPostRequest(d)
-	cluster, apiResponse, err := c.sdkClient.KafkaApi.ClustersPost(ctx).KafkaPostRequestBodyData(*request).Execute()
+	cluster, apiResponse, err := c.sdkClient.ClustersApi.ClustersPost(ctx).ClusterCreate(*request).Execute()
 	apiResponse.LogInfo()
 	return cluster, apiResponse, err
 }
@@ -35,54 +38,40 @@ func (c *Client) IsClusterAvailable(ctx context.Context, d *schema.ResourceData)
 	return strings.EqualFold(*Cluster.Metadata.State, constant.Available), nil
 }
 
-func (c *Client) UpdateCluster(ctx context.Context, id string, d *schema.ResourceData) (kafka.KafkaPostPutResponseData, utils.ApiResponseInfo, error) {
-	request := setClusterPatchRequest(d)
-	ClusterResponse, apiResponse, err := c.sdkClient.KafkaApi.ClustersPut(ctx, id).KafkaPutRequestBodyData(*request).Execute()
-	apiResponse.LogInfo()
-	return ClusterResponse, apiResponse, err
-}
-
 func (c *Client) DeleteCluster(ctx context.Context, id string) (utils.ApiResponseInfo, error) {
-	apiResponse, err := c.sdkClient.KafkaApi.ClustersDelete(ctx, id).Execute()
+	apiResponse, err := c.sdkClient.ClustersApi.ClustersDelete(ctx, id).Execute()
 	apiResponse.LogInfo()
 	return apiResponse, err
 }
 
 func (c *Client) IsClusterDeleted(ctx context.Context, d *schema.ResourceData) (bool, error) {
-	_, apiResponse, err := c.sdkClient.KafkaApi.ClustersFindById(ctx, d.Id()).Execute()
+	_, apiResponse, err := c.sdkClient.ClustersApi.ClustersFindById(ctx, d.Id()).Execute()
 	apiResponse.LogInfo()
 	return apiResponse.HttpNotFound(), err
 }
 
-func (c *Client) GetClusterById(ctx context.Context, id string) (kafka.KafkaClusterByIdResponseData, utils.ApiResponseInfo, error) {
-	Cluster, apiResponse, err := c.sdkClient.KafkaApi.ClustersFindById(ctx, id).Execute()
+func (c *Client) GetClusterById(ctx context.Context, id string) (
+	kafka.ClusterRead, utils.ApiResponseInfo, error,
+) {
+	Cluster, apiResponse, err := c.sdkClient.ClustersApi.ClustersFindById(ctx, id).Execute()
 	apiResponse.LogInfo()
 	return Cluster, apiResponse, err
 }
 
-func (c *Client) ListClusters(ctx context.Context) (kafka.KafkaListResponseData, *kafka.APIResponse, error) {
-	Clusters, apiResponse, err := c.sdkClient.KafkaApi.ClustersGet(ctx).Execute()
+func (c *Client) ListClusters(ctx context.Context) (kafka.ClusterReadList, *kafka.APIResponse, error) {
+	Clusters, apiResponse, err := c.sdkClient.ClustersApi.ClustersGet(ctx).Execute()
 	apiResponse.LogInfo()
 	return Clusters, apiResponse, err
 }
 
-func setClusterPostRequest(d *schema.ResourceData) *kafka.KafkaPostRequestBodyData {
+func setClusterPostRequest(d *schema.ResourceData) *kafka.ClusterCreate {
 	clusterName := d.Get("name").(string)
 	version := d.Get("version").(string)
 	size := d.Get("size").(string)
-	return kafka.NewKafkaPostRequestBodyData(*kafka.NewKafkaCluster(clusterName, version, size))
+	return kafka.NewClusterCreate(*kafka.NewCluster(clusterName, version, size, nil))
 }
 
-func setClusterPatchRequest(d *schema.ResourceData) *kafka.KafkaPutRequestBodyData {
-	name := d.Get("name").(string)
-	version := d.Get("version").(string)
-	size := d.Get("size").(string)
-	request := kafka.NewKafkaPutRequestBodyData(*kafka.NewKafkaCluster(name, version, size))
-
-	return request
-}
-
-func (c *Client) SetKafkaClusterData(d *schema.ResourceData, cluster *kafka.KafkaClusterByIdResponseData) error {
+func (c *Client) SetKafkaClusterData(d *schema.ResourceData, cluster *kafka.ClusterRead) error {
 
 	if cluster.Id != nil {
 		d.SetId(*cluster.Id)
@@ -104,74 +93,31 @@ func (c *Client) SetKafkaClusterData(d *schema.ResourceData, cluster *kafka.Kafk
 				return err
 			}
 		}
+
+		if cluster.Properties.Connections != nil && len(*cluster.Properties.Connections) > 0 {
+			var connections []interface{}
+
+			for _, connection := range *cluster.Properties.Connections {
+				connectionEntry := c.SetConnectionProperties(connection)
+				connections = append(connections, connectionEntry)
+			}
+
+			if err := d.Set("connections", connections); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil
 }
 
-func (c *Client) SetKafkaClusterMetadata(d *schema.ResourceData, cluster *kafka.KafkaClusterByIdResponseData) error {
+func (c *Client) SetConnectionProperties(connection kafka.KafkaClusterConnection) map[string]interface{} {
+	connectionMap := map[string]interface{}{}
 
-	if cluster.Metadata == nil {
-		return fmt.Errorf("expected metadata in the response for the kafka Cluster with ID: %s, but received 'nil' instead", *cluster.Id)
-	}
-	if cluster.Metadata.ServerAddress != nil {
-		if err := d.Set("server_address", *cluster.Metadata.ServerAddress); err != nil {
-			return fmt.Errorf("error setting server_address: %w", err)
-		}
-	}
-	if cluster.Metadata.BrokerAddresses != nil {
-		if err := d.Set("broker_addresses", *cluster.Metadata.BrokerAddresses); err != nil {
-			return fmt.Errorf("error setting broker_addresses: %w", err)
-		}
+	utils.SetPropWithNilCheck(connectionMap, "datacenter_id", connection.DatacenterId)
+	utils.SetPropWithNilCheck(connectionMap, "lan_id", connection.LanId)
+	utils.SetPropWithNilCheck(connectionMap, "cidr", connection.Cidr)
+	utils.SetPropWithNilCheck(connectionMap, "broker_addresses", connection.BrokerAddresses)
 
-	}
-
-	return nil
+	return connectionMap
 }
-
-//
-// func (c *Client) SetClusterData(d *schema.ResourceData, Cluster kafka.Cluster) error {
-// 	d.SetId(*Cluster.Id)
-//
-// 	if Cluster.Properties == nil {
-// 		return fmt.Errorf("expected properties in the response for the kafka Cluster with ID: %s, but received 'nil' instead", *Cluster.Id)
-// 	}
-//
-// 	if Cluster.Metadata == nil {
-// 		return fmt.Errorf("expected metadata in the response for the kafka Cluster with ID: %s, but received 'nil' instead", *Cluster.Id)
-// 	}
-//
-// 	if Cluster.Properties.Name != nil {
-// 		if err := d.Set("name", *Cluster.Properties.Name); err != nil {
-// 			return utils.GenerateSetError(ClusterResourceName, "name", err)
-// 		}
-// 	}
-//
-// 	if Cluster.Properties.Logs != nil {
-// 		logs := make([]interface{}, len(*Cluster.Properties.Logs))
-// 		for i, logElem := range *Cluster.Properties.Logs {
-// 			// Populate the logElem entry.
-// 			logEntry := make(map[string]interface{})
-// 			logEntry["source"] = *logElem.Source
-// 			logEntry["tag"] = *logElem.Tag
-// 			logEntry["protocol"] = *logElem.Protocol
-// 			logEntry["public"] = *logElem.Public
-//
-// 			// Logic for destinations
-// 			destinations := make([]interface{}, len(*logElem.Destinations))
-// 			for i, destination := range *logElem.Destinations {
-// 				destinationEntry := make(map[string]interface{})
-// 				destinationEntry["type"] = *destination.Type
-// 				destinationEntry["retention_in_days"] = *destination.RetentionInDays
-// 				destinations[i] = destinationEntry
-// 			}
-// 			logEntry["destinations"] = destinations
-// 			logs[i] = logEntry
-// 		}
-// 		if err := d.Set("log", logs); err != nil {
-// 			return utils.GenerateSetError(ClusterResourceName, "log", err)
-// 		}
-// 	}
-//
-// 	return nil
-// }
