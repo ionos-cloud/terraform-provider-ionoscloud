a09dcfc5cdd077ce1ce79e867a9dee83af6b5018
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2024-07-23 12:15:42 +0300

    feat: service

--- services/nfs/cluster.go
+++ services/nfs/cluster.go
@@ -8,34 +8,33 @@ import (
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	sdk "github.com/ionos-cloud/sdk-go-nfs"
-
 	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/utils/constant"
 )
 
-// GetNFSClusterByID returns a cluster given an ID
-func (c *Client) GetNFSClusterByID(ctx context.Context, id string, location string) (sdk.ClusterRead, *sdk.APIResponse, error) {
-	cluster, apiResponse, err := c.Location(location).sdkClient.ClustersApi.ClustersFindById(ctx, id).Execute()
+// GetNFSClusterById returns a cluster given an ID
+func (c *Client) GetNFSClusterById(ctx context.Context, id string) (sdk.ClusterRead, *sdk.APIResponse, error) {
+	cluster, apiResponse, err := c.sdkClient.ClustersApi.ClustersFindById(ctx, id).Execute()
 	apiResponse.LogInfo()
 	return cluster, apiResponse, err
 }
 
 // ListNFSClusters returns a list of all clusters
-func (c *Client) ListNFSClusters(ctx context.Context, d *schema.ResourceData) (sdk.ClusterReadList, *sdk.APIResponse, error) {
-	clusters, apiResponse, err := c.Location(d.Get("location").(string)).sdkClient.ClustersApi.ClustersGet(ctx).Execute()
+func (c *Client) ListNFSClusters(ctx context.Context) (sdk.ClusterReadList, *sdk.APIResponse, error) {
+	clusters, apiResponse, err := c.sdkClient.ClustersApi.ClustersGet(ctx).Execute()
 	apiResponse.LogInfo()
 	return clusters, apiResponse, err
 }
 
 // DeleteNFSCluster deletes a cluster given an ID
-func (c *Client) DeleteNFSCluster(ctx context.Context, d *schema.ResourceData) (*sdk.APIResponse, error) {
-	apiResponse, err := c.Location(d.Get("location").(string)).sdkClient.ClustersApi.ClustersDelete(ctx, d.Id()).Execute()
+func (c *Client) DeleteNFSCluster(ctx context.Context, id string) (*sdk.APIResponse, error) {
+	apiResponse, err := c.sdkClient.ClustersApi.ClustersDelete(ctx, id).Execute()
 	apiResponse.LogInfo()
 	return apiResponse, err
 }
 
 // UpdateNFSCluster updates a cluster given an ID or creates a new one if it doesn't exist
 func (c *Client) UpdateNFSCluster(ctx context.Context, d *schema.ResourceData) (sdk.ClusterRead, *sdk.APIResponse, error) {
-	cluster, apiResponse, err := c.Location(d.Get("location").(string)).sdkClient.ClustersApi.ClustersPut(ctx, d.Id()).
+	cluster, apiResponse, err := c.sdkClient.ClustersApi.ClustersPut(ctx, d.Id()).
 		ClusterEnsure(*setClusterPutRequest(d)).Execute()
 	apiResponse.LogInfo()
 	return cluster, apiResponse, err
@@ -43,7 +42,7 @@ func (c *Client) UpdateNFSCluster(ctx context.Context, d *schema.ResourceData) (
 
 // CreateNFSCluster creates a new cluster
 func (c *Client) CreateNFSCluster(ctx context.Context, d *schema.ResourceData) (sdk.ClusterRead, *sdk.APIResponse, error) {
-	cluster, apiResponse, err := c.Location(d.Get("location").(string)).sdkClient.ClustersApi.ClustersPost(ctx).
+	cluster, apiResponse, err := c.sdkClient.ClustersApi.ClustersPost(ctx).
 		ClusterCreate(*setClusterPostRequest(d)).Execute()
 	apiResponse.LogInfo()
 	return cluster, apiResponse, err
@@ -73,14 +72,9 @@ func (c *Client) SetNFSClusterData(d *schema.ResourceData, cluster sdk.ClusterRe
 		}
 	}
 
-	if cluster.Properties.Nfs != nil {
-		nfs := []map[string]interface{}{
-			{
-				"min_version": *cluster.Properties.Nfs.MinVersion,
-			},
-		}
-		if err := d.Set("nfs", nfs); err != nil {
-			return fmt.Errorf("error setting nfs data for the NFS Cluster with ID %s: %w", *cluster.Id, err)
+	if cluster.Properties.Nfs != nil && cluster.Properties.Nfs.MinVersion != nil {
+		if err := d.Set("min_version", *cluster.Properties.Nfs.MinVersion); err != nil {
+			return err
 		}
 	}
 
@@ -106,30 +100,28 @@ func (c *Client) SetNFSClusterData(d *schema.ResourceData, cluster sdk.ClusterRe
 // IsClusterReady checks if the cluster is ready
 func (c *Client) IsClusterReady(ctx context.Context, d *schema.ResourceData) (bool, error) {
 	clusterID := d.Id()
-	location := d.Get("location").(string)
-	cluster, _, err := c.GetNFSClusterByID(ctx, clusterID, location)
+	cluster, _, err := c.GetNFSClusterById(ctx, clusterID)
 	if err != nil {
-		return false, fmt.Errorf("failed checking if Cluster %s from %s is ready: %w", clusterID, location, err)
+		return true, fmt.Errorf("status check failed for Cluster ID: %v, error: %w", clusterID, err)
 	}
 
 	if cluster.Metadata == nil || cluster.Metadata.Status == nil {
 		return false, fmt.Errorf("metadata or status is empty for Cluster ID: %v", clusterID)
 	}
 
-	log.Printf("[INFO] state of the cluster with ID %s is: %s", clusterID, *cluster.Metadata.Status)
+	log.Printf("[INFO] state of the cluster with ID %s is: %s ", clusterID, *cluster.Metadata.Status)
 	return strings.EqualFold(*cluster.Metadata.Status, constant.Available), nil
 }
 
 // IsClusterDeleted checks if the cluster is deleted
 func (c *Client) IsClusterDeleted(ctx context.Context, d *schema.ResourceData) (bool, error) {
 	clusterID := d.Id()
-	location := d.Get("location").(string)
-	_, apiResponse, err := c.GetNFSClusterByID(ctx, clusterID, location)
+	_, apiResponse, err := c.GetNFSClusterById(ctx, clusterID)
 	if err != nil {
 		if apiResponse.HttpNotFound() {
 			return true, nil
 		}
-		return false, fmt.Errorf("failed checking if Cluster %s from %s has been deleted: %w", clusterID, location, err)
+		return false, fmt.Errorf("check failed for Cluster deletion status, ID: %v, error: %w", clusterID, err)
 	}
 	return false, nil
 }
@@ -148,38 +140,31 @@ func setClusterPutRequest(d *schema.ResourceData) *sdk.ClusterEnsure {
 func setClusterConfig(d *schema.ResourceData) sdk.Cluster {
 	name := d.Get("name").(string)
 	size := int32(d.Get("size").(int))
-
-	nfsRaw := d.Get("nfs").([]interface{})
-	nfs := sdk.ClusterNfs{}
-	var minVersion string
-	if len(nfsRaw) > 0 && nfsRaw[0] != nil {
-		nfsData := nfsRaw[0].(map[string]interface{})
-		minVersion = nfsData["min_version"].(string)
-
-		nfs = sdk.ClusterNfs{
-			MinVersion: &minVersion,
-		}
-	}
-
+	minVersion := d.Get("min_version").(string)
 	connectionsRaw := d.Get("connections").([]interface{})
-	connections := make([]sdk.ClusterConnections, 1)
-	if len(connectionsRaw) > 0 && connectionsRaw[0] != nil {
-		connData := connectionsRaw[0].(map[string]interface{})
+
+	connections := make([]sdk.ClusterConnections, len(connectionsRaw))
+	for i, conn := range connectionsRaw {
+		connData := conn.(map[string]interface{})
 		datacenterID := connData["datacenter_id"].(string)
 		lan := connData["lan"].(string)
 		ipAddress := connData["ip_address"].(string)
 
-		connections[0] = sdk.ClusterConnections{
+		connectionObj := sdk.ClusterConnections{
 			DatacenterId: &datacenterID,
 			Lan:          &lan,
 			IpAddress:    &ipAddress,
 		}
+
+		connections[i] = connectionObj
 	}
 
 	return sdk.Cluster{
-		Name:        &name,
-		Size:        &size,
-		Nfs:         &nfs,
+		Name: &name,
+		Size: &size,
+		Nfs: &sdk.ClusterNfs{
+			MinVersion: &minVersion,
+		},
 		Connections: &connections,
 	}
 }
