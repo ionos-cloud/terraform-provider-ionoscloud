280a8df3fef44fbf663d0b22a004c01acc7d0b2f
Radu Mocanu <radu_mocanu2000@yahoo.com>
2024-08-01 15:14:23 +0300

    revert check for err != nil

--- ionoscloud/resource_cdn_distribution.go
+++ ionoscloud/resource_cdn_distribution.go
@@ -133,10 +133,10 @@ func resourceCDNDistributionCreate(ctx context.Context, d *schema.ResourceData, 
 		distribution.Properties.CertificateId = &attrStr
 	}
 
-	if routingRules, err := cdnService.GetRoutingRulesData(d); err != nil {
-		return diag.FromErr(err)
-	} else {
+	if routingRules, err := cdnService.GetRoutingRulesData(d); err == nil {
 		distribution.Properties.RoutingRules = routingRules
+	} else {
+		return diag.FromErr(err)
 	}
 
 	createdDistribution, _, err := client.SdkClient.DistributionsApi.DistributionsPost(ctx).DistributionCreate(distribution).Execute()
@@ -195,10 +195,10 @@ func resourceCDNDistributionUpdate(ctx context.Context, d *schema.ResourceData, 
 		request.Properties.CertificateId = &attrStr
 	}
 
-	if routingRules, err := cdnService.GetRoutingRulesData(d); err != nil {
-		return diag.FromErr(err)
-	} else {
+	if routingRules, err := cdnService.GetRoutingRulesData(d); err == nil {
 		request.Properties.RoutingRules = routingRules
+	} else {
+		return diag.FromErr(err)
 	}
 
 	_, _, err := client.SdkClient.DistributionsApi.DistributionsPut(ctx, d.Id()).DistributionUpdate(request).Execute()
