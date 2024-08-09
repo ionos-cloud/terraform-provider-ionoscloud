54b2aebffb51e01ee7f1b432ee99965f26f4e774
Radu Mocanu <radu_mocanu2000@yahoo.com>
2024-08-09 17:46:22 +0300

    fix: check if no registies are found because none exist, wrong user agent for vm autoscaling

--- services/autoscaling/client.go
+++ services/autoscaling/client.go
@@ -27,7 +27,7 @@ func NewClient(username, password, token, url, version, terraformVersion string)
 	newAutoscalingConfig.MaxWaitTime = constant.MaxWaitTime
 	newAutoscalingConfig.HTTPClient = &http.Client{Transport: utils.CreateTransport()}
 	newAutoscalingConfig.UserAgent = fmt.Sprintf(
-		"terraform-provider/%s_ionos-cloud-sdk-go-logging/%s_hashicorp-terraform/%s_terraform-plugin-sdk/%s_os/%s_arch/%s",
+		"terraform-provider/%s_ionos-cloud-sdk-go-vm-autoscaling/%s_hashicorp-terraform/%s_terraform-plugin-sdk/%s_os/%s_arch/%s",
 		version, autoscaling.Version, terraformVersion, meta.SDKVersionString(), runtime.GOOS, runtime.GOARCH) //nolint:staticcheck
 
 	return &Client{sdkClient: autoscaling.NewAPIClient(newAutoscalingConfig)}
