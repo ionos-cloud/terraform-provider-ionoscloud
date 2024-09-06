780c09d5253585ec9c6f9d4b28466bf34d01357e
digna-ionos <darius-andrei.igna@ionos.com>
2024-09-06 11:47:07 +0300

    imports fix improve comment

--- xpprovider/provider.go
+++ xpprovider/provider.go
@@ -2,13 +2,15 @@ package xpprovider
 
 import (
 	fwprovider "github.com/hashicorp/terraform-plugin-framework/provider"
+
 	internalfwProvider "github.com/ionos-cloud/terraform-provider-ionoscloud/v6/internal/framework/provider"
 	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/ionoscloud"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-// GetProvider is used to get the provider for crossplane upjet provider
+// GetProvider exports the two providers sdkv2 and framework
+// Used in crossplane upjet ionoscloud provider
 func GetProvider() (fwprovider.Provider, *schema.Provider) {
 	return internalfwProvider.New(), ionoscloud.Provider()
 }
