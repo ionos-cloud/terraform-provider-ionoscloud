a7a03617eac848f3a8450f4df17b965b1a278355
cguran-ionos <cristian-mihai.guran@ionos.com>
2024-11-08 14:28:54 +0200

    Update services/cloudapi/nsg/nsg.go
    Co-authored-by: Deatcu Andrei-Cristian <andrei-cristian.deatcu@ionos.com>

--- services/cloudapi/nsg/nsg.go
+++ services/cloudapi/nsg/nsg.go
@@ -37,7 +37,7 @@ func (nsg *Service) PutServerNSG(ctx context.Context, dcID, serverID string, raw
 	if err != nil {
 		return diag.FromErr(err)
 	}
-	if errState := cloudapi.WaitForStateChange(ctx, nsg.Meta, nsg.D, apiResponse, schema.TimeoutCreate); errState != nil {
+	if errState := cloudapi.WaitForStateChange(ctx, nsg.Meta, nsg.D, apiResponse, schema.TimeoutUpdate); errState != nil {
 		if cloudapi.IsRequestFailed(errState) {
 			nsg.D.SetId("")
 		}
