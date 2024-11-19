c395d349b368c7c665e760eb6d7f767ad690f308
Sören König <537955+skoenig@users.noreply.github.com>
2024-11-19 17:59:59 +0100

    Docs additional examples (#706)

---
+++ examples/api_gateway_cdn/variables.tf
@@ -0,0 +1,9 @@
+variable "DOMAIN" {
+  description = "Top-level domain name for the API Gateway"
+  type        = string
+}
+
+variable "CERT_REQUESTER" {
+  description = "Email address of the certificate requester"
+  type        = string
+}
