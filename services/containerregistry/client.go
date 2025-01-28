c9b6927b3928003562dfe7e6bf751ac3b15a364f
adeatcu-ionos <andrei-cristian.deatcu@ionos.com>
2025-01-28 16:58:25 +0200

    refactor: Use bundle instead of sdk-go-container-registry

--- services/containerregistry/client.go
+++ services/containerregistry/client.go
@@ -3,36 +3,33 @@ package containerregistry
 import (
 	"fmt"
 	"net/http"
-	"os"
 	"runtime"
 
 	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/utils/constant"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/meta"
-	cr "github.com/ionos-cloud/sdk-go-container-registry"
+	cr "github.com/ionos-cloud/sdk-go-bundle/products/containerregistry/v2"
+	"github.com/ionos-cloud/sdk-go-bundle/shared"
 
 	"github.com/ionos-cloud/terraform-provider-ionoscloud/v6/utils"
 )
 
 type Client struct {
-	sdkClient *cr.APIClient
+	sdkClient cr.APIClient
 }
 
 func NewClient(username, password, token, url, version, terraformVersion string, insecure bool) *Client {
-	newConfigRegistry := cr.NewConfiguration(username, password, token, url)
+	newConfigRegistry := shared.NewConfiguration(username, password, token, url)
 
-	if os.Getenv(constant.IonosDebug) != "" {
-		newConfigRegistry.Debug = true
-	}
 	newConfigRegistry.MaxRetries = constant.MaxRetries
 	newConfigRegistry.MaxWaitTime = constant.MaxWaitTime
-
-	newConfigRegistry.HTTPClient = &http.Client{Transport: utils.CreateTransport(insecure)}
 	newConfigRegistry.UserAgent = fmt.Sprintf(
 		"terraform-provider/%s_ionos-cloud-sdk-go-container-cr/%s_hashicorp-terraform/%s_terraform-plugin-sdk/%s_os/%s_arch/%s",
 		version, cr.Version, terraformVersion, meta.SDKVersionString(), runtime.GOOS, runtime.GOARCH)
-
-	return &Client{
-		sdkClient: cr.NewAPIClient(newConfigRegistry),
+	client := &Client{
+		sdkClient: *cr.NewAPIClient(newConfigRegistry),
 	}
+	client.sdkClient.GetConfig().HTTPClient = &http.Client{Transport: utils.CreateTransport(insecure)}
+
+	return client
 }
