87b1c624ec20cef06f4b9a6ddb6d4925aee6435e
cristi <cristian.mihai.guran@ionos.com>
2025-02-25 15:40:31 +0200

    fix: revert push

--- services/objectstoragemanagement/accesskeys.go
+++ services/objectstoragemanagement/accesskeys.go
@@ -55,9 +55,6 @@ func (c *Client) modifyConfigURL() {
 func (c *Client) GetAccessKey(ctx context.Context, accessKeyID string) (objectstoragemanagement.AccessKeyRead, *objectstoragemanagement.APIResponse, error) {
 	c.modifyConfigURL()
 	accessKey, apiResponse, err := c.client.AccesskeysApi.AccesskeysFindById(ctx, accessKeyID).Execute()
-	if apiResponse.HttpNotFound() {
-		return accessKey, apiResponse, nil
-	}
 	apiResponse.LogInfo()
 	return accessKey, apiResponse, err
 }
