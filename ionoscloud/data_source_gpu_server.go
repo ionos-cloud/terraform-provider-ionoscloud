da40ad7bb40803719de62b5d613806543396bf6b
Alexandru Claudius Virtopeanu <alexandru-claudius.virtopeanu@ionos.com>
2025-12-16 17:05:50 +0200

    feat: add gpu server datasource

---
+++ ionoscloud/data_source_gpu_server.go
@@ -0,0 +1,186 @@
+package ionoscloud
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+func dataSourceGpuServer() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceCubeServerRead,
+		Schema: map[string]*schema.Schema{
+			"template_uuid": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"datacenter_id": {
+				Type:             schema.TypeString,
+				Required:         true,
+				ValidateDiagFunc: validation.ToDiagFunc(validation.StringIsNotWhiteSpace),
+			},
+			"id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+			"hostname": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"availability_zone": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"vm_state": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cpu_family": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"boot_cdrom": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"boot_volume": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"boot_image": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"token": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cdroms": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     cdromsServerDSResource,
+			},
+			"volumes": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"availability_zone": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"image_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"image_password": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ssh_keys": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Schema{
+								Type: schema.TypeString,
+							},
+						},
+						"bus": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"licence_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cpu_hot_plug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"ram_hot_plug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"nic_hot_plug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"nic_hot_unplug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"disc_virtio_hot_plug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"disc_virtio_hot_unplug": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"device_number": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"pci_slot": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"backup_unit_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"user_data": {
+							Type:     schema.TypeString,
+							Optional: true,
+						},
+						"boot_server": {
+							Type:        schema.TypeString,
+							Description: "The UUID of the attached server.",
+							Computed:    true,
+						},
+						"require_legacy_bios": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "Indicates if the image requires the legacy BIOS for compatibility or specific needs.",
+						},
+					},
+				},
+			},
+			"nics": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem:     nicServerDSResource,
+			},
+			"security_groups_ids": {
+				Type:     schema.TypeList,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+				Computed: true,
+			},
+			"ram": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"cores": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+		},
+		Timeouts: &resourceDefaultTimeouts,
+	}
+}
