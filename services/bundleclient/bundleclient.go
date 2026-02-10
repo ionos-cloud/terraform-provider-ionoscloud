1f4bbeeb6a62bbe11b91e27ca7b254340e7133d2
Gabriela Limberea <gabriela.limberea@ionos.com>
2026-02-10 18:21:45 +0200

    add config struct to replace common client with per-operation client for cloudapi

--- services/bundleclient/bundleclient.go
+++ services/bundleclient/bundleclient.go
@@ -31,6 +31,7 @@ func New(clientOptions clientoptions.TerraformClientOptions, fileConfig *filecon
 		AutoscalingClient:             autoscalingService.NewClient(clientOptions, fileConfig),
 		CertManagerClient:             cert.NewClient(clientOptions, fileConfig),
 		CloudApiClient:                cloudapi.NewClient(clientOptions, fileConfig),
+		CloudAPIConfig:                cloudapi.NewConfig(clientOptions, fileConfig),
 		ContainerClient:               crService.NewClient(clientOptions, fileConfig),
 		DNSClient:                     dnsService.NewClient(clientOptions, fileConfig),
 		LoggingClient:                 loggingService.NewClient(clientOptions, fileConfig),
@@ -51,6 +52,7 @@ func New(clientOptions clientoptions.TerraformClientOptions, fileConfig *filecon
 // SdkBundle is a struct that defines the bundle client. It is used for both sdkv2 and plugin framework
 type SdkBundle struct {
 	CloudApiClient                *ionoscloud.APIClient
+	CloudAPIConfig                *cloudapi.Config
 	InMemoryDBClient              *inmemorydb.Client
 	PsqlClient                    *dbaasService.PsqlClient
 	MongoClient                   *dbaasService.MongoClient
