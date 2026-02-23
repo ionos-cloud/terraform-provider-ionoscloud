6e166e71e41f1f1ed2fa28ff43d265627533b783
Radu-Ionut Mocanu <radu-ionut.mocanu@ionos.com>
2026-02-23 15:07:14 +0200

    refactor: move to a different package

---
+++ utils/diags/diags.go
@@ -0,0 +1,83 @@
+package diags
+
+import (
+	"errors"
+	"fmt"
+	"net/url"
+	"regexp"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+var requestIDPattern = regexp.MustCompile(`/requests/([-A-Fa-f0-9]+)/`)
+
+// DiagsOpts contains optional fields for enhancing error messages.
+type DiagsOpts struct {
+	Timeout            string
+	ResourceNameString string // The schema key for the resource name (e.g., "name", "display_name")
+	RequestLocation    *url.URL
+	StatusCode         int // HTTP status code from API response; 0 means not available
+}
+
+// buildErrorMessage constructs an enriched error message with resource context.
+func buildErrorMessage(d *schema.ResourceData, err string, opts *DiagsOpts) string {
+	targetField := "name"
+	if opts != nil && opts.ResourceNameString != "" {
+		targetField = opts.ResourceNameString
+	}
+
+	var sb strings.Builder
+
+	sb.WriteString("Error: ")
+	sb.WriteString(err)
+
+	if opts != nil && opts.StatusCode >= 500 {
+		sb.WriteString("\nThis is an API Error. Please contact API support.")
+	}
+
+	if d.Id() != "" {
+		fmt.Fprintf(&sb, "\nResource ID: %s", d.Id())
+	}
+
+	if v, ok := d.GetOk(targetField); ok {
+		if nameVal, isString := v.(string); isString {
+			fmt.Fprintf(&sb, "\nResource Name: %s", nameVal)
+		}
+	}
+
+	if opts != nil {
+		if opts.Timeout != "" {
+			fmt.Fprintf(&sb, "\nConfigured timeout: %s", opts.Timeout)
+		}
+		if opts.RequestLocation != nil {
+			reqID := extractRequestId(opts.RequestLocation.String())
+			if reqID != "" {
+				fmt.Fprintf(&sb, "\nRequest ID: %s", reqID)
+			}
+		}
+	}
+
+	return sb.String()
+}
+
+// ToDiags wraps an error into a Terraform diagnostic with additional context.
+func ToDiags(d *schema.ResourceData, err string, opts *DiagsOpts) diag.Diagnostics {
+	return diag.FromErr(errors.New(buildErrorMessage(d, err, opts)))
+}
+
+// ToError wraps an error message with resource context, similar to ToDiags but returns an error.
+// This is used in import functions and helper functions that return (T, error) instead of diag.Diagnostics.
+func ToError(d *schema.ResourceData, err string, opts *DiagsOpts) error {
+	return errors.New(buildErrorMessage(d, err, opts))
+}
+
+// extractRequestId parses the HTTP header string to find the UUID request ID.
+func extractRequestId(loc string) string {
+	matches := requestIDPattern.FindStringSubmatch(loc)
+	if len(matches) > 1 {
+		return matches[1]
+	}
+	return ""
+}
