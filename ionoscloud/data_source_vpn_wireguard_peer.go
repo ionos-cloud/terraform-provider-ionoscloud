4e1b67269f0ecc92790e9959d1a19919c7551f83
cristi <cristian.mihai.guran@ionos.com>
2026-03-27 18:14:32 +0200

    fix: dataSourceVpnWireguardPeerRead checks

--- ionoscloud/data_source_vpn_wireguard_peer.go
+++ ionoscloud/data_source_vpn_wireguard_peer.go
@@ -80,8 +80,6 @@ func dataSourceVpnWireguardPeerRead(ctx context.Context, d *schema.ResourceData,
 	idValue, idOk := d.GetOk("id")
 	nameValue, nameOk := d.GetOk("name")
 	location := d.Get("location").(string)
-	id := idValue.(string)
-	name := nameValue.(string)
 
 	if idOk && nameOk {
 		return diag.FromErr(fmt.Errorf("ID and name cannot be both specified at the same time"))
@@ -93,11 +91,13 @@ func dataSourceVpnWireguardPeerRead(ctx context.Context, d *schema.ResourceData,
 	var peer vpnSdk.WireguardPeerRead
 	var err error
 	if idOk {
+		id := idValue.(string)
 		peer, _, err = client.GetWireguardPeerByID(ctx, gatewayID, id, location)
 		if err != nil {
 			return diag.FromErr(fmt.Errorf("an error occurred while fetching the WireGuard Peer with ID: %s, error: %w", id, err))
 		}
 	} else {
+		name := nameValue.(string)
 		var results []vpnSdk.WireguardPeerRead
 		peers, _, err := client.ListWireguardPeers(ctx, gatewayID, location)
 		if err != nil {
